78b722a02fbc5e51442aafa4b56e5015d760325c
coolasjake <coolasjake8@gmail.com>
2024-04-13 22:50:11 +1000

    Version change

--- Assets/Scripts/Minion.cs
+++ Assets/Scripts/Minion.cs
@@ -9,12 +9,27 @@ public class Minion : MonoBehaviour
 
     public void Work()
     {
+        if (assignedWorkstation == null)
+            return;
+
         float workSpeed = 1f;
 
         switch(assignedWorkstation.type)
         {
-            //case Workable.WorkType.Chalk:
-
+            case Workable.WorkType.Chalk:
+                workSpeed = data.mineSpeed;
+                break;
+            case Workable.WorkType.Inspiration:
+                workSpeed = data.researchSpeed;
+                break;
+            case Workable.WorkType.Circle:
+                workSpeed = data.mineSpeed;
+                break;
+            case Workable.WorkType.Blood:
+                //workSpeed = data.sacrificePower;
+                return;
+            case Workable.WorkType.None:
+                return;
         }
 
         assignedWorkstation.DoWork(workSpeed);
